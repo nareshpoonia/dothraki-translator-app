4d529dae9c27206b816d200615d7bfe73eef14ff
Naresh <68911787+nareshpoonia@users.noreply.github.com>
2021-09-08 22:30:22 +0530

    docs: updated script

---
+++ app.js
@@ -0,0 +1,32 @@
+var button = document.querySelector("#button");
+var inputTextArea = document.querySelector("#input-text-area");
+var outputText = document.querySelector("#output-text");
+
+//api Url
+var serverURL = "https://api.funtranslations.com/translate/dothraki.json";
+
+// function to take input and change the url
+function getTranslatedText(text) {
+  return serverURL + "?" + "text=" + text;
+}
+
+// function to handle error
+function errorHandler(error) {
+  console.log("There is an error, please try again after some time");
+  alert("There is an error, please try again after some time");
+}
+
+//Event Handler for click event
+button.addEventListener("click", function callback() {
+  // .value saves the value of input
+  var inputText = inputTextArea.value;
+
+  fetch(getTranslatedText(inputText))
+    .then((response) => response.json())
+    .then((json) => {
+      // consoling json helps to decide the code
+      var translatedText = json.contents.translated;
+      outputText.innerText = translatedText;
+    })
+    .catch(errorHandler);
+});
